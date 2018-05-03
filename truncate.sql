f9024335774d41784fd2863ac98afaf183e36593
Salingga Yusrianda <salgadd@gmail.com>
2018-05-04 02:23:19 +0700

    Menambahkan truncate.sql
    Untuk men-truncate database

--- truncate.sql
+++ truncate.sql
@@ -2,11 +2,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE table polls_choice;
 TRUNCATE table polls_question;
 TRUNCATE table polls_information;
-SET FOREIGN_KEY_CHECKS = 1;
 
 TRUNCATE TABLE posts_attachment;
 DELETE FROM auth_user WHERE id != 1;
 TRUNCATE TABLE posts_tag;
+TRUNCATE TABLE posts_category;
 TRUNCATE TABLE posts_post;
-TRUNCATE TABLE posts_posttag;
+TRUNCATE TABLE posts_post_tags;
+TRUNCATE TABLE posts_post_categories;
+SET FOREIGN_KEY_CHECKS = 1;
 
