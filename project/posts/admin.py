be27a733eeaea9a937e8b40fdffe546a2531b522
Salingga Yusrianda <salgadd@gmail.com>
2018-05-04 02:25:44 +0700

    Menambahkan admin dan models pada posts

--- project/posts/admin.py
+++ project/posts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from tags_input import admin as tags_input_admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Post, Tag, Attachment, Category
+
+
+class PostAdmin(tags_input_admin.TagsInputAdmin):
+    list_display = ('title', 'author', 'publish')
+    list_display_links = ('title', )
+    list_filter = ('publish', 'author')
+    search_fields = ('title', )
+    tag_fields = ('tags', 'categories')
+
+
+UserAdmin.list_display = ('email', 'first_name', 'last_name', 'is_active', 'date_joined', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
+admin.site.register(Attachment)
+admin.site.register(Category)
--- project/posts/models.py
+++ project/posts/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import AbstractUser
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    parent = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
@@ -18,14 +29,16 @@ class Attachment(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    content = models.TextField(null=True)
+    author = models.ForeignKey(User, null=True, related_name="author", on_delete=models.SET_NULL)
+    editor = models.ForeignKey(User, null=True, related_name="editor", on_delete=models.SET_NULL)
+    content = RichTextField(null=True)
     url = models.TextField(null=True)
+    slug = models.SlugField(unique=True, null=True, max_length=200)
+    categories = models.ManyToManyField(Category)
+    tags = models.ManyToManyField(Tag) 
+    image = models.ImageField(null=True)
     attachment = models.ForeignKey(Attachment, null=True, on_delete=models.CASCADE)
     publish = models.BooleanField(default=False)
+    draft = models.BooleanField(default=False)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     pub_date = models.DateTimeField('date published')
-
-
-class PostTag(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
