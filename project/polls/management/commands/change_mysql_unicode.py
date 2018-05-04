278677724febb5123a3a516909f185ae1fa24969
Salingga Yusrianda <salgadd@gmail.com>
2018-05-05 02:03:41 +0700

    Fix change_mysql_unicode params

--- project/polls/management/commands/change_mysql_unicode.py
+++ project/polls/management/commands/change_mysql_unicode.py
@@ -1,17 +1,20 @@
 #! /usr/bin/env python
 from django.core.management.base import BaseCommand, CommandError
+from telusur.settings import DATABASES
 import MySQLdb
 
 class Command(BaseCommand):
     help = 'Handle MySQL unicode problem.'
 
     def handle(self, *args, **options):
-        host = "localhost"
-        passwd = "566856"
-        user = "root"
-        dbname = "production_telusur"
+        db_credential = DATABASES.get('default')
+        dbname = db_credential.get('NAME')
 
-        db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=dbname)
+        db = MySQLdb.connect(
+            host='localhost', 
+            user=db_credential.get('USER'), 
+            passwd=db_credential.get('PASSWORD'), 
+            db=dbname)
         cursor = db.cursor()
 
         cursor.execute("ALTER DATABASE `%s` CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci'" % dbname)
