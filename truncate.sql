682528130e501ac7a1c3a43cbd8a6897dfe0ed39
Salingga Yusrianda <salgadd@gmail.com>
2018-05-12 08:28:35 +0700

    Mengganti truncate.sql untuk keperluan migrasi wordpress

--- truncate.sql
+++ truncate.sql
@@ -2,9 +2,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE table polls_choice;
 TRUNCATE table polls_question;
 TRUNCATE table polls_information;
+DELETE FROM auth_user WHERE id != 1;
 
 TRUNCATE TABLE posts_attachment;
-DELETE FROM auth_user WHERE id != 1;
 TRUNCATE TABLE posts_tag;
 TRUNCATE TABLE posts_category;
 TRUNCATE TABLE posts_post;
