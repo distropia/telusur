917c49af7536d530554459830790a3887d5942ce
Wahyu Rismawan <wahyu@badr-interactive.com>
2018-08-18 10:41:48 +0700

    slicing template (part 1)

--- project/telusur/views.py
+++ project/telusur/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-
+from django.http import HttpResponse
 
 def index(request):
-    
-    return render(request, 'politics/index.html')
+    return render(request, 'pages/home/home-index.html')
