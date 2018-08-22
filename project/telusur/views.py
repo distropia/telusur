d628fc71e6d2715b11ececc66c1870f8db659dc8
Wahyu Rismawan <wahyurismawan@gmail.com>
2018-08-22 15:58:59 +0700

    section 1: latest news @TODO: load images.

--- project/telusur/views.py
+++ project/telusur/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from posts.models import Post
+import logging
+import json
+
+logger = logging.getLogger(__name__)
 
 def index(request):
-    return render(request, 'pages/home/home-index.html')
+    posts = Post.objects.all().order_by('-pub_date')[:10]
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'pages/home/home-index.html', context)
