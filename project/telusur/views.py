222c2f2a49f896bd2d5e38bc46c1b04e7ea8f413
Wahyu Rismawan <wahyurismawan@gmail.com>
2018-08-22 16:48:03 +0700

    most viewed posts

--- project/telusur/views.py
+++ project/telusur/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse, JsonResponse
-from posts.models import Post
+from posts.models import Post, Category
 import logging
 import json
 
 logger = logging.getLogger(__name__)
 
 def index(request):
-    posts = Post.objects.all().order_by('-pub_date')[:10]
+    latest_posts = Post.objects.all().order_by('-pub_date')[:10]
+    most_viewed_posts = Post.objects.all().order_by('-views')[:10]
+    categories = Category.objects.all().exclude(id=1)[:5]
 
     context = {
-        'posts': posts
+        'latest_posts': latest_posts,
+        'most_viewed_posts': most_viewed_posts,
+        'categories': categories
     }
 
     return render(request, 'pages/home/home-index.html', context)
