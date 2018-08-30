66053d75be9b4f556aff26f33486d4244956ccf3
Salingga Yusrianda <salgadd@gmail.com>
2018-08-30 20:36:17 +0700

    Revert "Merge branch 'async-tasks' into develop"
    This reverts commit f1e7d96378250cf984bd23521b9a4577118dc639.

--- project/posts/urls.py
+++ project/posts/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from . import views, schedulers
+from . import views
 
 app_name = 'posts'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('generate/', schedulers.GenerateRandomUserView.as_view(), name='generate_posts'),
-    path('users_list/', schedulers.UserListView.as_view(), name='users_list')
 ]
