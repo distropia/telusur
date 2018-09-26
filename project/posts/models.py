32e53a7d5548fecbd55b1591dc8d9a02ae143624
Salingga Yusrianda <salgadd@gmail.com>
2018-09-27 00:05:32 +0700

    fix url and slugify on saving post

--- project/posts/models.py
+++ project/posts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
+from datetime import datetime
 
 
 class Tag(models.Model):
@@ -33,19 +35,26 @@ class Attachment(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(User, null=True, related_name="author", on_delete=models.SET_NULL)
-    editor = models.ForeignKey(User, null=True, related_name="editor", on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, editable=False, related_name="author", on_delete=models.SET_NULL)
+    editor = models.ForeignKey(User, null=True, editable=False, related_name="editor", on_delete=models.SET_NULL)
     content = RichTextField(null=True)
-    url = models.TextField(null=True)
-    slug = models.SlugField(unique=True, null=True, max_length=200)
+    url = models.TextField(null=True, editable=False)
+    slug = models.SlugField(unique=True, null=True, max_length=200, editable=False)
     categories = models.ManyToManyField(Category)
     tags = models.ManyToManyField(Tag) 
     attachment = models.ForeignKey(Attachment, null=True, on_delete=models.CASCADE)
     publish = models.BooleanField(default=False)
     draft = models.BooleanField(default=False)
-    views = models.IntegerField(null=True, default=0)
+    views = models.IntegerField(null=True, default=0, editable=False)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.title
+
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        if self.pk is None:
+            now = datetime.now()
+            self.url = str(now.year) + "/" + str(now.month) + "/" + str(now.day) + "/" + slugify(self.title)
+            self.slug = slugify(self.title)
+        super(Post, self).save(force_insert, force_update, *args, **kwargs)
